ac13656377a3a11247533c484c36c40b949ed0bf
Mikhail <test@test.com>
2023-12-18 21:08:42 +0300

    реализовал работу с данными во фронт части приложения

---
+++ src/components/ToLearnWords.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'TheMarkedWords',
+};
+</script>
+
+<template>
+  <div class="table caption-top" style="width: 100%">
+    <p>Список всех слов для изучения</p>
+    <div class="table-title">
+      <p style="width: 50px; text-align: center">#</p>
+      <p style="width: 100%; text-align: center">слово на английском</p>
+      <p style="width: 100%; text-align: center">перевод</p>
+    </div>
+    <div class="table-body">
+      <div class="table-row" v-for="(word, index) in $store.getters.toLearn" :key="word.id">
+        <p style="width: 50px; text-align: center">{{ index + 1 }}</p>
+        <p style="width: 100%; text-align: center">{{ word.enText }}</p>
+        <p style="width: 100%; text-align: center">{{ word.ruText }}</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/TrainingPage.vue
+++ src/views/TrainingPage.vue
@@ -3,8 +3,18 @@ import CardComponent from '../components/CardComponent.vue';
 export default {
   name: 'TrainingPage',
   components: {
-    CardComponent
-  }
+    CardComponent,
+  },
+  data() {
+    return {
+      words: this.$store.getters.allWords,
+    };
+  },
+  methods: {
+    nextWord() {
+      this.$store.commit('nextWord');
+    },
+  },
 };
 </script>
 
@@ -15,8 +25,51 @@ export default {
         Если Вы знаете слово нажмите на кнопку "знаю" и оно отметится, если же Вам слово не известно нажмите кнопку "не
         знаю" и увидите перевод. Кнопка далее покажет следующее слово для изучения
       </p>
-      <card-component></card-component>
-      <button type="button" class="btn btn-primary next-btn mb-3">перейти к следующему слову</button>
+      <div class="card-wrapper">
+        <div class="v-card-carousel">
+          <div v-for="(word, index) in words" :key="word.id" style="max-width: 500px">
+            <transition name="fade">
+              <card-component 
+                :enText="word.enText" 
+                :ruText="word.ruText"
+                :id="word.id"
+                :marked="word.marked"
+                v-if="index === this.$store.getters.countWords"
+              ></card-component>
+            </transition>
+          </div>
+        </div>
+      </div>
+
+      <button 
+        type="button" 
+        class="btn btn-primary next-btn mb-3" 
+        @click="nextWord"
+        >
+        перейти к следующему слову
+      </button>
     </div>
   </section>
 </template>
+
+<style>
+.card-wrapper {
+  margin: auto;
+  max-width: 480px;
+  overflow: hidden;
+}
+.v-card-carousel {
+  display: flex;
+  justify-content: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
